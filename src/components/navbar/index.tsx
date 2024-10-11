5b5c4c7dba1db9f1498494a7659f5f4ff34d131f
yash7xm <yash.7xm@gmail.com>
2024-10-11 19:12:18 +0530

    navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { CommandMenu } from "./commandMenu";
 import { Input } from "@/components/ui/input";
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { MagnifyingGlassIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
 
 function Navbar() {
     const [open, setOpen] = useState(false);
@@ -11,9 +11,9 @@ function Navbar() {
     };
 
     return (
-        <div className="rounded-lg border px-4 py-2 flex items-center">
+        <div className="rounded-lg border px-4 py-2 flex items-center justify-between">
             <div className="flex items-center">
-                <div>Vyzon</div>
+                <div className="cursor-pointer">Vyzon</div>
 
                 <div className="w-64 ml-16 border flex rounded-md items-center pl-4">
                     <MagnifyingGlassIcon className="mr-2 h-4 w-4 shrink-0 opacity-50" />
@@ -22,6 +22,18 @@ function Navbar() {
                         onFocus={handleInputFocus}
                         className="cursor-pointer border-none px-0"
                     />
+                </div>
+            </div>
+
+            <div className="flex gap-8 items-center mr-8">
+                <div className="cursor-pointer text-sm hover:underline">
+                    Documentation
+                </div>
+                <div className="cursor-pointer text-sm hover:underline">
+                    Playground
+                </div>
+                <div className="cursor-pointer">
+                    <GitHubLogoIcon className="size-5" />
                 </div>
             </div>
 
