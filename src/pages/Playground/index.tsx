0ad305ca133eadc4240bebd3536a14d2eaa1d376
yash7xm <yash.7xm@gmail.com>
2025-06-08 13:03:33 +0530

    fix: playground

--- src/pages/Playground/index.tsx
+++ src/pages/Playground/index.tsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
 import Navbar from "@/components/navbar";
 import { PlaygroundResizable } from "@/components/playground/playgroundResizable";
 
 function Playground() {
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-
     return (
         <div className="w-screen h-screen p-5 flex flex-col gap-6">
             <div className="w-full">
-                <Navbar toggleSidebar={() => setSidebarOpen(prev => !prev)} />
+                <Navbar toggleSidebar={() => {}} />
             </div>
 
             <div className="w-full h-full overflow-hidden">
